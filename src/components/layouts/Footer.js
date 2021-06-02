38cc45d554bfa6c16ef544ccab2f88ef003b6430
x1lh3art <x1lh3art@gmail.com>
2021-06-02 21:08:37 +0000

    imp

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,22 +1,9 @@
 import React from 'react'
-import prismicLogo from '../../images/logo-prismic.svg'
 
 export default () => (
   <footer className="container">
     <p>
-      Proudly published with
-      {' '}
-      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-        Prismic
-      </a>
-      <br />
-      <a href="https://prismic.io" target="_blank" rel="noopener noreferrer">
-        <img
-          className="footer-logo"
-          src={prismicLogo}
-          alt="Gray Prismic logo"
-        />
-      </a>
+      Copyright Dollarspedia 2021. All Rights Reserved.
     </p>
   </footer>
 )
